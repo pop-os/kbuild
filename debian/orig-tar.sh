c164ca72563015e06a36d8dc9374d78b79cb2864
Gianfranco Costamagna <locutusofborg@debian.org>
2023-04-19 10:35:32 +0200

    Update orig-tar script

--- debian/orig-tar.sh
+++ debian/orig-tar.sh
@@ -4,18 +4,18 @@ set -e
 
 SVNROOT=$1
 VERSION=$2
-REVISION=`svn info $SVNROOT | grep "Last Changed Rev:" | cut -d' ' -f4`
+REVISION=`svn info ${SVNROOT} | grep "Last Changed Rev:" | cut -d' ' -f4`
 
-DIR=kbuild-$REVISION
-TAR=../kbuild_$VERSION.orig.tar.gz
+DIR=kbuild-${REVISION}
+TAR=../kbuild_${VERSION}.orig.tar.gz
 
-svn co -r $REVISION $SVNROOT $DIR
-tar -c -z  --exclude '*/src/kWorker/tests-gpl2/*' --exclude '*/kBuild/bin*' --exclude '*/out/*' --exclude '*/.svn*' --exclude '*/src/kmk/doc/make.texi' -f $TAR $DIR
-rm -rf $DIR
+svn co -r ${REVISION} ${SVNROOT} ${DIR}
+tar -c -z  --exclude '*/src/kWorker/tests-gpl2/*' --exclude '*/kBuild/bin*' --exclude '*/out/*' --exclude '*/.svn*' --exclude '*/src/kmk/doc/make.texi' -f ${TAR} ${DIR}
+rm -rf ${DIR}
 
 # move to directory 'tarballs'
 if [ -r .svn/deb-layout ]; then
   . .svn/deb-layout
-  mv $TAR $origDir
-  echo "moved $TAR to $origDir"
+  mv ${TAR} ${origDir}
+  echo "moved ${TAR} to ${origDir}"
 fi
